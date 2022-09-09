cb17907a7175038a7341ee7c9ca8082b63d55417
cc <cc@cc.com>
2022-09-09 14:26:04 +0530

    pdf design

--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,7 @@ export default function Home() {
   const [selectedPaitentId,setSelectedPaitentId] = useState('')
   const [selectedSession,setSelectedSession] = useState('')
   const [graphData ,setGraphData] = useState([])
+  const [paitentPdfData, setPaitentPdfData] = useState({})
 
 // Plot Images
   const [imagePlot1, setPlotImage1]= useState('')
@@ -75,7 +76,7 @@ const [testingImage, setTestingImage] =  useState('Hi there this testing text')
     setLoading(true)
     getPatientService(selectedUserId)
     .then(data=>{
-      console.log(data.data)
+     
       setPaitents(data.data)
       setLoading(false)
     })
@@ -89,7 +90,7 @@ const [testingImage, setTestingImage] =  useState('Hi there this testing text')
     setLoading(true);
     getPatientSessionService(selectedPaitentId)
     .then(data=>{
-      console.log(data)
+     
     
       setSessionDates(data.data)
       setLoading(false)
@@ -106,18 +107,19 @@ const [testingImage, setTestingImage] =  useState('Hi there this testing text')
   }
 
   const testingPdfDownlaod = ()=>{
+    console.log(paitentPdfData)
 
     const doc  = new jsPDF();
     doc.setFont("centred", "normal");
     doc.setFontSize(11);
-    doc.text("Paciente: Marcos", 20, 30, null, null,);
+    doc.text("Paciente: "+` ${paitentPdfData.name}`, 20, 30, null, null,);
 
     doc.setFontSize(11);
     
     doc.addImage("./images/logo.png", "png", 175, 5, 30, 15);
 
     doc.setFontSize(11);
-    doc.text("Fecha de la visita: 24-05-2022", 20, 40, null, null,);
+    doc.text("Fecha de la visita: "+` ${paitentPdfData.painDuration}`, 20, 40, null, null,);
 
     doc.setFontSize(11);
     doc.text("Hora: 08:56", 100, 40, null, null,);
@@ -133,13 +135,13 @@ const [testingImage, setTestingImage] =  useState('Hi there this testing text')
     doc.text("Edad", 20, 70);
 
     doc.setFontSize(11);
-    doc.text("40", 40, 70);
+    doc.text(`${paitentPdfData.age}`, 40, 70);
 
     doc.setFontSize(11);
     doc.text("Sexo", 20, 75);
 
     doc.setFontSize(11);
-    doc.text("Hombre", 40, 75);
+    doc.text(`${paitentPdfData.sex}`, 40, 75);
 
     doc.setFontSize(11);
     doc.text("Trabajo", 20, 80);
@@ -152,49 +154,49 @@ const [testingImage, setTestingImage] =  useState('Hi there this testing text')
     doc.text("Peso", 120, 70);
 
     doc.setFontSize(11);
-    doc.text("82", 140, 70);
+    doc.text(`${paitentPdfData.weight}`, 140, 70);
 
     doc.setFontSize(11);
     doc.text("Altura", 120, 75);
 
     doc.setFontSize(11);
-    doc.text("180", 140, 75);
+    doc.text(`${paitentPdfData.height}`, 140, 75);
 
     doc.setFontSize(11);
     doc.text("Hobby", 120, 80);
 
     doc.setFontSize(11);
-    doc.text("Natación", 140, 80);
+    doc.text(`${paitentPdfData.hobby}`, 140, 80);
 
     doc.text("¿Dónde tiene el dolor?", 20, 100);
 
-    doc.text("Rodilla izquierda por el lado interior y parte trasera", 23, 110);
+    doc.text(`${paitentPdfData.painLocation}`, 23, 110);
     doc.setLineWidth(0.1);
     doc.rect(20, 105, 165, 25);
 
     doc.text("¿Qué tipo de dolor?", 20, 140);
 
-     doc.text("Pinchazos en el ligamento, al sobrecargarse", 23, 150);
+     doc.text(`${paitentPdfData.painType}`, 23, 150);
     doc.setLineWidth(0.1);
     doc.rect(20, 145, 165, 25);
 
     doc.text("¿En qué situaciones le duele?", 20, 180);
     
-    doc.text("Cuando se levanta de la silla y está frio, hasta que se calienta", 23, 190);
+    doc.text(`${paitentPdfData.painSituation}`, 23, 190);
     doc.setLineWidth(0.1);
     doc.rect(20, 185, 165, 25);
 
 
     doc.text("¿Desde cuándo le duele?", 20, 220);
     
-    doc.text("Desde hace dos semanas que empezó a jugar al Baloncesto", 23, 230);
+    doc.text(`${paitentPdfData.painDuration}`, 23, 230);
     doc.setLineWidth(0.1);
     doc.rect(20, 225, 165, 25);
 
 
     doc.text("Lesiones previas:", 20, 260);
 
-    doc.text("Hernias discales en la columna", 23, 270);
+    doc.text(`${paitentPdfData.previousInjury}`, 23, 270);
     doc.setLineWidth(0.1);
     doc.rect(20, 265, 165, 25);
    
@@ -238,9 +240,9 @@ const [testingImage, setTestingImage] =  useState('Hi there this testing text')
 
 
    doc.text("Presiones promedio", 20, 120);
-   doc.addImage("./images/l3.png", "png", 50, 122, 80, 80);
+   doc.addImage("./images/l3.png", "png", 50, 122, 40, 70);
    
-   doc.addImage("./images/r4.png", "png", 100, 122, 80, 80);
+   doc.addImage("./images/r4.png", "png", 100, 122, 40, 70);
    
 
    doc.addPage();
@@ -255,15 +257,31 @@ const [testingImage, setTestingImage] =  useState('Hi there this testing text')
 
   doc.addImage("./images/logo.png", "png", 175, 5, 30, 15);
 
- doc.text("Observación clínica", 20, 30);
+ doc.text("Diagnóstico", 20, 30);
  doc.setLineWidth(0.60);
- doc.line(20, 33, 180, 33);
+ doc.line(20, 32, 180, 32);
+ 
+ doc.text("Debido al dolor izquierdo en la rodilla, realiza un apoyo mayor en la pierna derecha", 20, 38);
 
+ doc.text("Recomendación de tratamiento", 20, 53);
+ doc.setLineWidth(0.60);
+ doc.line(20, 55, 180, 55);
 
-
+ doc.text("Debido al dolor izquierdo en la rodilla, realiza un apoyo mayor en la pierna derecha", 20, 60);
     doc.output('dataurlnewwindow');
 
 
+
+  }
+
+  const setSelectedPaitenetData = (pid) =>{
+    setSelectedPaitentId(pid)
+    for(let i = 0; i<paitents.length; i++){
+      if(paitents[i].id == pid){
+        setPaitentPdfData(paitents[i]);
+      }
+    
+    }
 
   }
 
@@ -307,13 +325,13 @@ const [testingImage, setTestingImage] =  useState('Hi there this testing text')
       <div class="mt-3 form-group">
         <label>Select patient for get detail </label>
         <div className='input-group mb-3 mt-1'>
-          <select onChange={e => setSelectedPaitentId(e.target.value) }  className='form-control'>
+          <select onChange={e => setSelectedPaitenetData(e.target.value) }  className='form-control'>
           <option value='' selected>Select Paitent</option>
             { paitents.map((user, index) => (
                 <option key={index} value={user.id}>{user.name}</option>
             ))}
         </select>
-        <button onClick={getSessionDates} type="button" className='btn btn-primary'>Get Session</button>
+        <button onClick={()=>getSessionDates()} type="button" className='btn btn-primary'>Get Session</button>
       </div>
       </div>
 
